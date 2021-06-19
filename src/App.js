296703499b50081e46af1bb9c52ae31b24db2234
skarfie123 <rahulpai@rocketmail.com>
2021-06-19 12:04:42 +0100

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Card from 'react-bootstrap/Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { url: '' };
+
+    this.handleUrlChange = this.handleUrlChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUrlChange(event) {
+    this.setState({ url: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.url);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Random YouTube</h1>
+        <p>Generate random selections from specific YouTube channels</p>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="url-label">YouTube Channel URL</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="https://www.youtube.com/snl"
+            aria-label="url"
+            aria-describedby="url-label"
+            onChange={this.handleUrlChange}
+          />
+          <InputGroup.Append>
+            <Button type="submit" variant="primary" onClick={this.handleSubmit}>Load</Button>
+          </InputGroup.Append>
+        </InputGroup>
+
+        <div className="row">
+
+          <Card className="column video">
+            <Card.Img variant="top" src="https://img.youtube.com/vi/gAYL5H46QnQ/0.jpg" />
+            <Card.Body>
+              <Card.Title>Video Title</Card.Title>
+              <Card.Text>
+                Description
+              </Card.Text>
+              <Button variant="primary" target="_blank" href="https://www.youtube.com/watch?v=gAYL5H46QnQ">Open</Button>
+            </Card.Body>
+          </Card>
+          <div className="column">
+            <Button variant="primary">Next</Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
